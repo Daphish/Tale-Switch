7552f1aeccc80fcf92b33190219ec93d282db8d0
Daphish <ivan.davila.2003@gmail.com>
2025-05-23 16:27:01 -0600

    barras de vida

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var health_bar = $HealthBar
+var hp = 100
 
 func _ready():
 	animated_sprite.connect("animation_finished", _on_animation_finished)
+	health_bar.value = hp
 
 func _physics_process(delta):
 	pass
 
 func take_damage():
+	hp -= 5
+	if hp <= 0:
+		get_tree().change_scene_to_file("res://scenes/second_instruction.tscn")
+	health_bar.value = hp
 	if animated_sprite.animation != "receive-damage":
 		animated_sprite.play("receive-damage")
 
